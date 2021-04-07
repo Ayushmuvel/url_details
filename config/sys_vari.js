ba92dfaee9f8a7a72f62bb5041caf39bc1a5e711
Ayush Muvel <63338883+Ayushmuvel@users.noreply.github.com>
2021-04-08 00:33:30 +0530

    Delete config directory

--- config/sys_vari.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    conn_port : 3000,
-    // mongo_url : 'mongodb://localhost:27017/contactlist',
-    mongo_url :"mongodb+srv://DbCollection:DbCollection@dbcollection.0yhf6.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
-}
